ab28427a00e777d6abd3456242a5ead07d7a3eb6
Mustafa Güvenç <mustafa_gvnc@windowslive.com>
2023-04-28 16:47:50 +0300

    Input Screen edited.

--- app/src/main/java/com/mustafaguvenc/kotlincomposeinstagram/ui/theme/Theme.kt
+++ app/src/main/java/com/mustafaguvenc/kotlincomposeinstagram/ui/theme/Theme.kt
@@ -13,9 +13,12 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
+
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
+
+
 
     /* Other default colors to override
     background = Color.White,
--- app/src/main/java/com/mustafaguvenc/kotlincomposeinstagram/view/InputScreen.kt
+++ app/src/main/java/com/mustafaguvenc/kotlincomposeinstagram/view/InputScreen.kt
@@ -1,10 +1,13 @@
 package com.mustafaguvenc.kotlincomposeinstagram.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -16,8 +19,11 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -41,21 +47,39 @@ fun InputScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.secondary)
+            .background(Color.Black)
             .padding(15.dp)
             .clickable {
                 focusManager.clearFocus()
             },
         contentAlignment = Alignment.Center,
     ) {
+        Column (Modifier.fillMaxHeight()){
+            Column (Modifier.fillMaxHeight().weight(2f), verticalArrangement = Arrangement.Center){
+                Text(
+                    text = "MGtagram", modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    textAlign = TextAlign.Center,
+                    fontSize = 44.sp,
+                    fontFamily = FontFamily.Cursive,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+
+                    )
+            }
 
 
-        Column {
+
+        Column (Modifier.fillMaxHeight().weight(4f)){
 
             TextField(value = emailInput,
                 onValueChange ={
                     emailInput = it
-            }, placeholder = { Text(text = "Enter Email...")},
+            }, placeholder = {
+                    Text(text = "Enter Email...",
+                    fontFamily = FontFamily.Cursive
+                    )},
                 modifier = Modifier
                     .fillMaxWidth()
                     .shadow(5.dp, CircleShape)
@@ -67,10 +91,11 @@ fun InputScreen(
                     unfocusedIndicatorColor = Color.Transparent),
                 label = {
                     Text(
-                        text = "Email"
+                        text = "Email",
+                        fontFamily = FontFamily.Cursive
                     )
                 },
-                maxLines = 2,
+                maxLines = 1
             )
 
             Spacer(modifier = Modifier.height(15.dp))
@@ -78,7 +103,10 @@ fun InputScreen(
             TextField(value = passwordInput,
                 onValueChange ={
                     passwordInput = it
-                }, placeholder = { Text(text = "Enter Password...")},
+                }, placeholder = {
+                    Text(text = "Enter Password...",
+                    fontFamily = FontFamily.Cursive
+                    )},
                 modifier = Modifier
                     .fillMaxWidth()
                     .shadow(5.dp, CircleShape)
@@ -89,13 +117,56 @@ fun InputScreen(
                     unfocusedIndicatorColor = Color.Transparent),
                 label = {
                     Text(
-                        text = "Password"
+                        text = "Password",
+                        fontFamily = FontFamily.Cursive
                     )
                 },
-                maxLines = 2
+                maxLines = 1
             )
 
             Spacer(modifier = Modifier.height(15.dp))
+
+            Row( modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)) {
+                Button(onClick = {
+
+                }, shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(horizontal = 5.dp).border(
+                            width = 2.dp,
+                            color = Color.White,
+                            shape = RoundedCornerShape(10.dp)
+                        )
+
+                ) {
+                    Text(text = "Sign In",
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Cursive
+                        )
+                }
+                Button(onClick = {
+
+                }, shape = RoundedCornerShape(20.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(horizontal = 5.dp)
+                        .border(
+                            width = 2.dp,
+                            color = Color.White,
+                            shape = RoundedCornerShape(10.dp)
+                        )) {
+                    Text(text = "Sign Up",
+                        fontSize = 20.sp,
+                        fontFamily = FontFamily.Cursive
+                        )
+                }
+            }
+
+
 
 
 
@@ -122,6 +193,7 @@ fun InputScreen(
 
              */
 
+        }
         }
     }
 }
