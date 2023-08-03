c347eb6a7500db2da830bd02fdfe98a3d59c4558
Mustafa Güvenç <mustafa_gvnc@windowslive.com>
2023-08-03 16:36:11 +0300

    upload screen 'de foto yükleme çalışılıyor

--- app/src/main/java/com/mustafaguvenc/kotlincomposeinstagram/view/UploadScreen.kt
+++ app/src/main/java/com/mustafaguvenc/kotlincomposeinstagram/view/UploadScreen.kt
@@ -1,8 +1,10 @@
 package com.mustafaguvenc.kotlincomposeinstagram.view
 
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.util.Log
 import android.view.KeyEvent
 import android.widget.Toast
 import androidx.compose.foundation.Image
@@ -51,16 +53,20 @@ import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
-import com.mustafaguvenc.kotlincomposeinstagram.Manifest
 import com.mustafaguvenc.kotlincomposeinstagram.R
 import com.mustafaguvenc.kotlincomposeinstagram.viewmodel.InputViewModel
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UploadScreen(
     navController: NavController,
     viewModel : InputViewModel
 ) {
+
+    val scaffoldState = rememberScaffoldState() // this contains the `SnackbarHostState`
+    val coroutineScope = rememberCoroutineScope()
 
     var commentInput by remember {
         mutableStateOf("")
@@ -70,170 +76,199 @@ fun UploadScreen(
     val focusManager = LocalFocusManager.current
     val context = LocalContext.current as Activity
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black)
-            .padding(15.dp)
-            .clickable {
-                focusManager.clearFocus()
-            }
-            ,
-        contentAlignment = Alignment.Center,
-    ) {
-        Column (Modifier.fillMaxHeight()){
-            Column (
-                Modifier
-                    .fillMaxHeight()
-                    .weight(1f), verticalArrangement = Arrangement.Center) {
-                Text(
-                    text = "Select Image",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(20.dp),
-                    textAlign = TextAlign.Center,
-                    fontSize = 35.sp,
-                    fontFamily = FontFamily.Cursive,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White,
+    Scaffold(modifier = Modifier,
+        scaffoldState = scaffoldState,
+        ) {
 
-                    )
-            }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+                .padding(15.dp)
+                .clickable {
+                    focusManager.clearFocus()
+                },
+            contentAlignment = Alignment.Center,
+        ) {
+            Column(Modifier.fillMaxHeight()) {
+                Column(
+                    Modifier
+                        .fillMaxHeight()
+                        .weight(1f), verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Select Image",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(20.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 35.sp,
+                        fontFamily = FontFamily.Cursive,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
 
-            Column (
+                        )
+                }
 
-                Modifier
-                    .fillMaxHeight()
-                    .weight(2f), verticalArrangement = Arrangement.Center) {
-                Image(painterResource(id = R.drawable.add_159647),"",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clickable {
-                            if (ContextCompat.checkSelfPermission(
-                                    context,
-                                    android.Manifest.permission.READ_EXTERNAL_STORAGE
-                                ) != PackageManager.PERMISSION_GRANTED
-                            ) {
-                                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                                        context,
-                                        android.Manifest.permission.READ_EXTERNAL_STORAGE
-                                    )
-                                ) {
-                                    Snackbar() {
-                                        
+                Column(
+
+                    Modifier
+                        .fillMaxHeight()
+                        .weight(2f), verticalArrangement = Arrangement.Center
+                ) {
+
+
+                    Image(painterResource(id = R.drawable.add_159647), "",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable {
+
+                                if(ContextCompat.checkSelfPermission(context,android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+                                    if(ActivityCompat.shouldShowRequestPermissionRationale(context,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+
+                                        coroutineScope.launch {
+                                            // using the `coroutineScope` to `launch` showing the snackbar
+                                            // taking the `snackbarHostState` from the attached `scaffoldState`
+                                            val snackbarResult =
+                                                scaffoldState.snackbarHostState.showSnackbar(
+                                                    message = "This is your message",
+                                                    actionLabel = "Do something."
+                                                )
+                                            when (snackbarResult) {
+                                                SnackbarResult.Dismissed -> Log.d(
+                                                    "SnackbarDemo",
+                                                    "Dismissed"
+                                                )
+                                                SnackbarResult.ActionPerformed -> Log.d(
+                                                    "SnackbarDemo",
+                                                    "Snackbar's button clicked"
+                                                )
+                                            }
+                                        }
+
+
                                     }
                                 }
-                            }
-                        })
-
-            }
-
-            Column (
-                Modifier
-                    .fillMaxHeight()
-                    .weight(2f), verticalArrangement = Arrangement.Center) {
-
-                OutlinedTextField(value = commentInput,
-                    onValueChange ={
-                        commentInput = it
-                    },
-                    placeholder = {
-                        Text(text = "Enter Comment...",
-                            fontFamily = FontFamily.Cursive
-                        )},
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .shadow(5.dp, CircleShape)
-                        .background(Color.White, CircleShape)
-                        .padding(horizontal = 20.dp, vertical = 12.dp)
-                        .focusRequester(focusRequester)
-                        //textfield da enter a basıldığında focus temizleniyordu.
-                        .onKeyEvent {
-                            if (it.key.equals(Key.Enter)) {
-                                focusRequester.requestFocus()
-                                return@onKeyEvent true
-                            }
-                            false
-                        }
 
 
-                       ,
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Color.White,
-                        unfocusedIndicatorColor = Color.Transparent),
-                    label = {
-                        Text(
-                            text = "Comment",
-                            fontFamily = FontFamily.Cursive
-                        )
-                    },
-                    trailingIcon = {
-                        IconButton(onClick = { focusManager.clearFocus() }) {
-                            Icon(
-                                imageVector =  Icons.Outlined.Done ,
-                                contentDescription = ""
+
+                            })
+
+
+                }
+
+
+
+                Column(
+                    Modifier
+                        .fillMaxHeight()
+                        .weight(2f), verticalArrangement = Arrangement.Center
+                ) {
+
+                    OutlinedTextField(value = commentInput,
+                        onValueChange = {
+                            commentInput = it
+                        },
+                        placeholder = {
+                            Text(
+                                text = "Enter Comment...",
+                                fontFamily = FontFamily.Cursive
                             )
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .shadow(5.dp, CircleShape)
+                            .background(Color.White, CircleShape)
+                            .padding(horizontal = 20.dp, vertical = 12.dp)
+                            .focusRequester(focusRequester)
+                            //textfield da enter a basıldığında focus temizleniyordu.
+                            .onKeyEvent {
+                                if (it.key.equals(Key.Enter)) {
+                                    focusRequester.requestFocus()
+                                    return@onKeyEvent true
+                                }
+                                false
+                            },
+                        colors = TextFieldDefaults.textFieldColors(
+                            backgroundColor = Color.White,
+                            unfocusedIndicatorColor = Color.Transparent
+                        ),
+                        label = {
+                            Text(
+                                text = "Comment",
+                                fontFamily = FontFamily.Cursive
+                            )
+                        },
+                        trailingIcon = {
+                            IconButton(onClick = { focusManager.clearFocus() }) {
+                                Icon(
+                                    imageVector = Icons.Outlined.Done,
+                                    contentDescription = ""
+                                )
+                            }
                         }
+
+
+                    )
+                }
+
+                Row(
+                    Modifier
+                        .fillMaxHeight()
+                        .weight(1f)
+                        .padding(20.dp), horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = {
+
+                    }, shape = RoundedCornerShape(20.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .padding(horizontal = 5.dp)
+                            .border(
+                                width = 2.dp,
+                                color = Color.White,
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .clickable {
+
+                            }
+
+                    ) {
+                        Text(
+                            text = "Upload",
+                            fontSize = 20.sp,
+                            fontFamily = FontFamily.Cursive
+                        )
                     }
+                    Button(onClick = {
+                        navController.navigate("feed_screen")
+                    }, shape = RoundedCornerShape(20.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                            .padding(horizontal = 5.dp)
+                            .border(
+                                width = 2.dp,
+                                color = Color.White,
+                                shape = RoundedCornerShape(10.dp)
+                            )
+                            .clickable {
 
+                            }
 
-
-                )
-            }
-
-            Row (
-                Modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .padding(20.dp), horizontalArrangement = Arrangement.Center) {
-                Button(onClick = {
-
-                }, shape = RoundedCornerShape(20.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                        .padding(horizontal = 5.dp)
-                        .border(
-                            width = 2.dp,
-                            color = Color.White,
-                            shape = RoundedCornerShape(10.dp)
+                    ) {
+                        Text(
+                            text = "Cancel",
+                            fontSize = 20.sp,
+                            fontFamily = FontFamily.Cursive
                         )
-                        .clickable {
-
-                        }
-
-                ) {
-                    Text(text = "Upload",
-                        fontSize = 20.sp,
-                        fontFamily = FontFamily.Cursive
-                    )
+                    }
                 }
-                Button(onClick = {
-                    navController.navigate("feed_screen")
-                }, shape = RoundedCornerShape(20.dp),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                        .padding(horizontal = 5.dp)
-                        .border(
-                            width = 2.dp,
-                            color = Color.White,
-                            shape = RoundedCornerShape(10.dp)
-                        )
-                        .clickable {
 
-                        }
 
-                ) {
-                    Text(text = "Cancel",
-                        fontSize = 20.sp,
-                        fontFamily = FontFamily.Cursive
-                    )
-                }
             }
-
-
-
         }
     }
 }
